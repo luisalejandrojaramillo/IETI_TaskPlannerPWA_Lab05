33335dbbd1bdeefde38e04fcd1c089691bae4118
Luis Alejandro Jaramillo <luisalejandrojaramillo@hotmail.com>
2020-09-23 20:47:33 -0500

    Cod de honor

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -20,6 +20,25 @@ const isLocalhost = Boolean(
     )
 );
 
+ 
+export function register(config) {
+  if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
+      // The URL constructor is available in all browsers that support SW.
+      const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href);
+      if (publicUrl.origin !== window.location.origin) {
+          // Our service worker won't work if PUBLIC_URL is on a different origin
+          // from what our page is served on. This might happen if a CDN is used to
+          // serve assets; see https://github.com/facebook/create-react-app/issues/2374
+          return;
+      }
+      //Add this part of code to your serviceWorker.js
+      window.addEventListener('install', () => {
+          console.log("install!!");
+      })
+    }
+  }
+
+/** 
 export function register(config) {
   if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
     // The URL constructor is available in all browsers that support SW.
@@ -52,7 +71,7 @@ export function register(config) {
       }
     });
   }
-}
+}*/
 
 function registerValidSW(swUrl, config) {
   navigator.serviceWorker
